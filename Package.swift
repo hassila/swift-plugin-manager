1d915b34952c416b01a46305098be003ba2700bb
Joakim Hassila <joj@mac.com>
2021-12-25 15:43:59 +0100

    Add dependency on swift-plugin version 0.1.0.

--- Package.swift
+++ Package.swift
@@ -29,7 +29,7 @@ let package = Package(
     dependencies: [
         .package(url: "https://github.com/apple/swift-system", from: "1.0.0"),
         .package(url: "https://github.com/apple/swift-log", from: "1.0.0"),
-        .package(url: "https://github.com/hassila/swift-plugin", branch: "main")
+        .package(url: "https://github.com/hassila/swift-plugin", from: "0.1.0")
     ],
     targets: [
         .target(
