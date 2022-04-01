aea45eaef11e85608e2b2d1d63097f0efa815c72
Joakim Hassila <joj@mac.com>
2022-04-01 14:16:13 +0200

    Add swift-docc-plugin dependency.

--- Package.swift
+++ Package.swift
@@ -29,7 +29,8 @@ let package = Package(
     dependencies: [
         .package(url: "https://github.com/apple/swift-system", from: "1.0.0"),
         .package(url: "https://github.com/apple/swift-log", from: "1.0.0"),
-        .package(url: "https://github.com/hassila/swift-plugin", from: "0.1.0")
+        .package(url: "https://github.com/hassila/swift-plugin", from: "0.1.0"),
+        .package(url: "https://github.com/apple/swift-docc-plugin", from: "1.0.0")
     ],
     targets: [
         .target(
